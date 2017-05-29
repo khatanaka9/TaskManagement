05c461278162f2a660beaa8877bb2c1fa0e1ec33
khatanaka9 <hatanaka@inazou.jp>
2017-05-29 11:11:13 +0900

    TODO追加とファイル名変更

--- app/controllers/ProjectControllers.java
+++ app/controllers/ProjectControllers.java
@@ -7,9 +7,9 @@ import play.mvc.*;
 
 public class ProjectControllers extends Controller {
 
-	public static void index() {
+	public static void projectList() {
+		// 一覧表示用にプロジェクトを全件取得
 		final List<Project> project = Project.findAll();
-		System.out.println(project.size());
 		render(project);
 	}
 
