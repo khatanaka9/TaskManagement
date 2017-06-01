4cc4abe8152621c13dc0041dfedcda130863dcaf
khatanaka9 <hatanaka@inazou.jp>
2017-06-01 17:08:10 +0900

    指摘点修正 TodoRepoのテスト作成

--- test/models/Todo/repo/TodoRepoTest.java
+++ test/models/Todo/repo/TodoRepoTest.java
@@ -1,6 +1,44 @@
 package models.Todo.repo;
 
-public class TodoRepoTest {
+import static org.hamcrest.CoreMatchers.*;
 
-	// TODO hatanaka 後で追加予定です
+import java.util.*;
+
+import models.Project.*;
+import models.Project.vo.*;
+import models.Todo.*;
+import models.Todo.vo.*;
+
+import org.junit.*;
+
+import play.test.*;
+
+public class TodoRepoTest extends UnitTest {
+
+	private TodoName todoname;
+	private ProjectName projectname;
+	private TodoContent content;
+	private Project project;
+	private Todo todo;
+
+	@Before
+	public void setup() {
+		Fixtures.deleteDatabase();
+		todoname = new TodoName("タスク1");
+		projectname = new ProjectName("案件1");
+		content = new TodoContent("内容1");
+		project = new Project(projectname).save();
+		todo = new Todo(project, todoname, content).save();
+		;
+
+	}
+
+	@Test
+	public void findByProjectTest() {
+		final List<Todo> todo1 = Todo.find("project = ? ", project).fetch();
+
+		// 1件のみのはず
+		assertThat(todo1.size(), is(1));
+
+	}
 }
